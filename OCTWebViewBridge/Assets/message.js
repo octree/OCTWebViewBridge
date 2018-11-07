20adb0f5907bc0b224e8ff12440c8cbb512eb7ec
Octree <fouljz@gmail.com>
2018-11-07 14:53:12 +0800

    🚧 update message.js

--- OCTWebViewBridge/Assets/message.js
+++ OCTWebViewBridge/Assets/message.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 //   core
+
 if (!window.bridge) {
-    
     window.bridge = {};
 }
 
@@ -9,7 +9,7 @@ window.bridge.callbackDispatcher = {
 __count: 0,
 cache: {},
 invoke: function invoke(id, args) {
-    var key = '' + id;
+    var key = "" + id;
     var func = window.bridge.callbackDispatcher.cache[key];
     func(args);
 },
@@ -18,19 +18,37 @@ push: function push(func) {
     if (func != null) {
         window.bridge.callbackDispatcher.__count += 1;
         index = window.bridge.callbackDispatcher.__count;
-        window.bridge.callbackDispatcher.cache['' + index] = func;
+        window.bridge.callbackDispatcher.cache["" + index] = func;
     }
     return index;
 }
 };
 
-window.bridge.invoke = function (id, selector, callback) {
-    for (var _len = arguments.length, args = Array(_len > 3 ? _len - 3 : 0), _key = 3; _key < _len; _key++) {
-        args[_key - 3] = arguments[_key];
+window.bridge.invoke = function(id, selector) {
+    for (
+         var _len = arguments.length,
+         args = Array(_len > 2 ? _len - 2 : 0),
+         _key = 2;
+         _key < _len;
+         _key++
+         ) {
+        args[_key - 2] = arguments[_key];
     }
     
-    var index = window.bridge.callbackDispatcher.push(callback);
-    window.webkit.messageHandlers.bridge.postMessage({ "identifier": id, "selector": selector, "callbackId": index, "args": args });
+    args = args.map(function(elt) {
+                    if (typeof v === "function") {
+                    return { type: 0, val: window.bridge.callbackDispatcher.push(elt) };
+                    } else {
+                    return { type: 1, val: elt };
+                    }
+                    });
+    
+    window.webkit.messageHandlers.bridge.postMessage({
+                                                     identifier: id,
+                                                     selector: selector,
+                                                     args: args
+                                                     });
 };
 
 window.bridge.plugin = {};
+
